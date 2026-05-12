9b4c963ff4d75682c6f8b2f148fe463aade68b47
ronaldojr8 <ronaldo.beltran@novare.com.ph>
2026-05-12 09:41:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ if time_to_insert:
     st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+#smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response)
 st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
